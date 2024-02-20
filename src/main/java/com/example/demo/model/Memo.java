c7bb7eff66af88352fcac9c72d23e884ceb0ee8f
gomachan3232 <gomachan3232@gmail.com>
2024-02-21 08:38:03 +0900

    Memoモデルの保存内容追記

--- src/main/java/com/example/demo/model/Memo.java
+++ src/main/java/com/example/demo/model/Memo.java
@@ -1,5 +1,31 @@
 package com.example.demo.model;
 
-public class Memo {
+import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class Memo {
+	//主キー、自動生成
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	//アカウントのユーザ名
+	private Account userName;
+	
+	//日付へ変換
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate Date;
+	
+	//必須入力、文字列が500文字まで
+	@NotBlank
+	@Size(max = 500)
+	private String memo;
+	
 }
