989c82f478e1f21dde4baf396d86309eeb437b8e
gomachan3232 <gomachan3232@gmail.com>
2024-02-23 08:14:13 +0900

    データローダの作成

---
+++ src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Account;
+import com.example.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements ApplicationRunner{
+	
+	private final PasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		//adminユーザを作成
+		var user = new Account();
+		user.setUsername("admin");
+		user.setPassword(passwordEncoder.encode("password"));
+		
+		userRepository.save(user);
+	}
+
+}
