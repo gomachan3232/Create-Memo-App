d6fc40e041b8d2a45436545033806a934960f483
gomachan3232 <gomachan3232@gmail.com>
2024-02-25 22:02:51 +0900

    Revert "Memoモデルの酒精"
    This reverts commit 20cccdc342046b948be680132c1c50c63e141b49.

--- src/main/java/com/example/demo/model/Memo.java
+++ src/main/java/com/example/demo/model/Memo.java
@@ -9,11 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Memo {
 	//主キー、自動生成
 	@Id
