20cccdc342046b948be680132c1c50c63e141b49
gomachan3232 <gomachan3232@gmail.com>
2024-02-25 21:58:33 +0900

    Memoモデルの酒精

--- src/main/java/com/example/demo/model/Memo.java
+++ src/main/java/com/example/demo/model/Memo.java
@@ -9,7 +9,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Memo {
 	//主キー、自動生成
 	@Id
