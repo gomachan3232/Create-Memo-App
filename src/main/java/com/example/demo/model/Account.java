e6d646895f710471ce99bdcfbed94696d40bedaa
gomachan3232 <gomachan3232@gmail.com>
2024-03-07 08:27:07 +0900

    Accountモデル保存条件修正、 DataLoaderADMINユーザのメール情報追加

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,8 +1,12 @@
 package com.example.demo.model;
 
+import com.example.demo.validator.UniqueLogin;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +15,11 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Account {
-	//必須入力、文字列が20文字まで
+	//必須入力、文字列が3文字以上12文字まで
+	@Id
 	@NotBlank
+	@Size(min = 3,max = 12)
+	@UniqueLogin
 	private String username;
 	
 	//必須入力、Email形式であること、文字列が200文字まで
@@ -21,8 +28,9 @@ public class Account {
 	@Size(max  = 200)
 	private String email;
 	
-	//
+	//必須入力、文字列が6文字以上60文字まで、半角英大文字小文字と数字を含む
 	@NotBlank
-	@Size(min = 6,max = 50)
+	@Size(min = 6,max = 60)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$", message = "半角英字大小と数字を含む値を設定してください")
 	private String password;
 }
