e8822896a263af8b32c0b83537af9891aa4fcb09
gomachan3232 <gomachan3232@gmail.com>
2024-06-02 17:59:28 +0900

    jsファイルの分割、パス名の変更

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,32 +1,3 @@
-//messageの要素を取得
-const message = document.querySelector("#message");
-message.animate({
-    opacity: [0, 1],
-}, 2000);
-
-window.setTimeout(() => {
-    message.style.opacity = 0;
-}, 5000);
-
-
-//TOPページの要素を取得（要素は複数あるため、querySelectorAllで取得）
-const topPageItems = document.querySelectorAll("#top-page-item");
-
-//取得した要素を繰り返し実行
-for(let i = 0; i < topPageItems.length; i++) {
-    const keyframes = {
-        opacity: [0, 1],
-        translate: ["0 50px", 0],
-    };
-    const options = {
-        duration: 1500,
-        delay: i * 500,
-        fill: "forwards",
-    };
-    topPageItems[i].animate(keyframes, options);
-}
-
-
 //memoページのメモ登録時の要素を取得
 const memoText = document.querySelector("#memo-text");
 //memoページのメモの文字数をカウントする要素を取得
